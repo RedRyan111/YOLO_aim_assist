fc0a8d2afd6e52e18dd99ccf7876962c5f242291
RedRyan111 <ravenengineerryan@gmail.com>
2023-12-05 00:51:53 -0600

    Refactoring

--- mouse_and_keyboard/button_presses.py
+++ mouse_and_keyboard/button_presses.py
@@ -1,22 +1,23 @@
 import ctypes
+import time
 
 
-class GameCommands():
+class GameCommands:
     def __init__(self):
         self.mouse = Mouse()
 
     def unpause_game(self):
-        mouse.set_position(-10000, 10000)
-        mouse.set_position(800, -10)
+        self.mouse.set_position(-10000, 10000)
+        self.mouse.set_position(800, -10)
         time.sleep(1)
-        mouse.left_click()
+        self.mouse.left_click()
         time.sleep(1)
-        mouse.click()
-
+        self.mouse.left_click()
 
     def shoot_at_position(self, x, y):
         self.mouse.set_position(x, y)
         self.mouse.left_click()
+
 
 class Mouse:
     def set_position(self, x, y):
