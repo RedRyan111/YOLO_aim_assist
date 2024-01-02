d52b7fa9730cbb92ebb83ee499154092299e8dea
RedRyan111 <ravenengineerryan@gmail.com>
2024-01-01 19:14:07 -0600

    Add ability to save images as gif

--- main.py
+++ main.py
@@ -1,49 +1,102 @@
+import glob
+import os
 from time import time
-
 import PIL.Image
+import numpy as np
 import torch
 from matplotlib import pyplot as plt
 from ultralytics import YOLO
 from ultralytics import RTDETR
 from plotting.plotting import draw_image
-
-
+from matplotlib import cm
+import cv2
+from PIL import Image
+import time
 
 torch.backends.cudnn.benchmark = True
-
-#maybe try tensorRT?
-
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-#model = YOLO('yolov8n-seg.pt').cuda()  #this is downscaling the image??
+# model = YOLO('yolov8n-seg.pt').cuda()  #this is downscaling the image??
 model = RTDETR('rtdetr-l.pt').cuda()
-
-test_img = 'BeforeObjectDetection.png'
-
-# Predict with the model
-for i in range(1):
-    a = time()
-    results = model(test_img)
-    b = time()
-    print(f'{i} time taken: {b - a}')
-print(f'------------------------------')
-print(type(results))
-print(len(results))
-#print(results[0].boxes)
-
-colors = []
-for i in range(15):
-    temp = torch.randint(low=0, high=255, size=[3])
-    colors.append(temp)
 
 colors = ['#1f77b4']
 
-# load video file
-# segment each image
-# ssave as video
 
-print(f'boxes: {dir(results[0].boxes)}')
+# Function to extract frames
+def FrameCapture(path):
+    vidObj = cv2.VideoCapture(path)
+    count = 0
+    success = 1
 
-img = draw_image(PIL.Image.open(test_img), results[0].boxes.xyxy, results[0].boxes.cls, colors)
-plt.imshow(img)
-plt.show()
+    while success:
+        if count > 400:# or count > 850:
+            continue
+        success, image = vidObj.read()
+
+        if image is None:
+            continue
+
+        print(f'image: {image.shape} type: {type(image)} min: {np.min(image)} max: {np.max(image)}')
+
+        image = image[:, :, [2, 1, 0]]
+
+        im = Image.fromarray(image).convert('RGB')
+        im.save("real_images/frame%d.jpg" % count)
+
+        results = model("real_images/frame%d.jpg" % count)
+
+        image = draw_image(PIL.Image.open("real_images/frame%d.jpg" % count), results[0].boxes.xyxy, results[0].boxes.cls, colors)
+
+        print(type(image))
+        image.save("test_images/frame%d.jpg" % count)
+
+        # saved = cv2.imwrite("frame%d.jpg" % count, image)
+
+        count += 1
+
+        print(f'count: {count}')
+
+
+path = 'C:/Users/raven/Videos/Left 4 Dead 2/Left 4 Dead 2 2021.01.12 - 22.43.44.08.DVR_Trim.mp4'
+FrameCapture(path)
+
+frame_folder = 'test_images'
+
+
+def make_gif(frame_folder):
+    frames = []
+    for i in range(1067):
+        filename = "test_images/frame%d.jpg" % i
+        isFile = os.path.isfile(filename)
+        if not isFile:
+            continue
+        image = Image.open(filename)
+        frames.append(image)
+
+
+    #frames = [Image.open(image) for image in glob.glob(f"{frame_folder}/*.jpg")]
+    print(len(frames))
+    frame_one = frames[0]
+    frame_one.save("my_awesome.gif", format="GIF", append_images=frames,
+                   save_all=True, duration=100, loop=0)
+
+
+def make_real_gif():
+    frames = []
+    for i in range(1067):
+        filename = "test_images/frame%d.jpg" % i
+        isFile = os.path.isfile(filename)
+        if not isFile:
+            continue
+        image = Image.open(filename)
+        frames.append(image)
+
+
+    #frames = [Image.open(image) for image in glob.glob(f"{frame_folder}/*.jpg")]
+    print(len(frames))
+    frame_one = frames[0]
+    frame_one.save("my_real.gif", format="GIF", append_images=frames,
+                   save_all=True, duration=100, loop=0)
+
+#make_gif(frame_folder)
+make_real_gif()
